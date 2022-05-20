d5f08d6ea1be7cee63c5343985c1061f3e61495b
Daniel Eckton <dseckton@gmail.com>
2022-05-19 18:35:50 -0600

    added POST request

--- controllers/builds.js
+++ controllers/builds.js
@@ -24,4 +24,34 @@ const getBuildById = async (req, res, next) => {
   });
 };
 
+const addBuild = async (req, res, next) => {
+  const build = {
+    game: req.body.game,
+    gameVersion: req.body.gameVersion,
+    buildName: req.body.buildName,
+    civilizations: req.body.civilizations,
+    maps: req.body.maps,
+    postedBy: req.body.postedBy,
+    postedDate: req.body.postedDate,
+    steps: req.body.steps,
+    videoExample: req.body.videoExample,
+  };
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection("build-orders")
+    .insertOne(build);
+  if (result.acknowledged) {
+    res.status(201).json(result);
+    next();
+  } else {
+    res
+      .status(500)
+      .json(
+        result.error ||
+          "Some error occurred while attempting to create new build."
+      );
+  }
+};
+
 module.exports = { getAllBuilds, getBuildById };
