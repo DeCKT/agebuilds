469f618d760f2cd4678be143d88bfa9dffec66fe
Daniel Eckton <dseckton@gmail.com>
2022-05-26 17:12:15 -0600

    adding try catch blocks

--- controllers/builds.js
+++ controllers/builds.js
@@ -36,85 +36,97 @@ const getBuildById = async (req, res, next) => {
 };
 
 const addBuild = async (req, res, next) => {
-  const build = {
-    game: req.body.game,
-    gameVersion: req.body.gameVersion,
-    buildName: req.body.buildName,
-    civilizations: req.body.civilizations,
-    maps: req.body.maps,
-    postedBy: req.body.postedBy,
-    postedDate: req.body.postedDate,
-    steps: req.body.steps,
-    videoExample: req.body.videoExample,
-  };
-  const result = await mongodb
-    .getDb()
-    .db()
-    .collection("build-orders")
-    .insertOne(build);
-  if (result.acknowledged) {
-    res.status(201).json(result);
-    next();
-  } else {
-    res
-      .status(500)
-      .json(
-        result.error ||
-          "Some error occurred while attempting to create new build."
-      );
+  try {
+    const build = {
+      game: req.body.game,
+      gameVersion: req.body.gameVersion,
+      buildName: req.body.buildName,
+      civilizations: req.body.civilizations,
+      maps: req.body.maps,
+      postedBy: req.body.postedBy,
+      postedDate: req.body.postedDate,
+      steps: req.body.steps,
+      videoExample: req.body.videoExample,
+    };
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection("build-orders")
+      .insertOne(build);
+    if (result.acknowledged) {
+      res.status(201).json(result);
+      next();
+    } else {
+      res
+        .status(500)
+        .json(
+          result.error ||
+            "Some error occurred while attempting to create new build."
+        );
+    }
+  } catch (err) {
+    res.status(400).json(err);
   }
 };
 
 const updateBuild = async (req, res, next) => {
-  const buildId = new req.params.id();
-  const result = await mongodb
-    .getDb()
-    .db()
-    .collection("build-orders")
-    .updateOne(
-      { _id: buildId },
-      {
-        $set: {
-          game: req.body.game,
-          gameVersion: req.body.gameVersion,
-          buildName: req.body.buildName,
-          civilizations: req.body.civilizations,
-          maps: req.body.maps,
-          postedBy: req.body.postedBy,
-          postedDate: req.body.postedDate,
-          steps: req.body.steps,
-          videoExample: req.body.videoExample,
-        },
-      }
-    );
-  if (result.acknowledged) {
-    res.status(200).json(result);
-    next();
-  } else {
-    res
-      .status(500)
-      .json(
-        result.error || "Some error occured while attempting to update build."
+  try {
+    const buildId = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection("build-orders")
+      .updateOne(
+        { _id: buildId },
+        {
+          $set: {
+            game: req.body.game,
+            gameVersion: req.body.gameVersion,
+            buildName: req.body.buildName,
+            civilizations: req.body.civilizations,
+            maps: req.body.maps,
+            postedBy: req.body.postedBy,
+            postedDate: req.body.postedDate,
+            steps: req.body.steps,
+            videoExample: req.body.videoExample,
+          },
+        }
       );
+    if (result.acknowledged) {
+      res.status(200).json(result);
+      next();
+    } else {
+      res
+        .status(500)
+        .json(
+          result.error || "Some error occured while attempting to update build."
+        );
+    }
+  } catch (err) {
+    res.staus(400).json(err);
   }
 };
 
 const deleteBuildById = async (req, res, next) => {
-  const buildId = new ObjectId(req.params.id);
-  const result = await mongodb
-    .getDb()
-    .db()
-    .collection("build-orders")
-    .deleteOne({ _id: buildId });
-  if (result.acknowledge) {
-    res.status(200).json(result);
-    next();
+  try {
+    const buildId = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection("build-orders")
+      .deleteOne({ _id: buildId });
+    if (result.acknowledge) {
+      res.status(200).json(result);
+      next();
+    }
+    res
+      .status(500)
+      .json(
+        result.error || "Some error occured while attempting to delete build."
+      );
+  } catch (err) {
+    res.status(400).json(err);
   }
-  res
-    .status(500)
-    .json(
-      result.error || "Some error occured while attempting to delete build."
-    );
 };
 
 module.exports = {
