30aea16983d5d31d37ad6570023f8ad386f47e70
Daniel Eckton <dseckton@gmail.com>
2022-05-28 16:49:28 -0600

    finalizing error handling

--- controllers/builds.js
+++ controllers/builds.js
@@ -92,31 +92,46 @@ const updateBuild = async (req, res, next) => {
     if (!req.params.id) {
       res.status(400).json("Build ID is required.");
     } else {
-      const result = await mongodb
-        .getDb()
-        .db()
-        .collection("build-orders")
-        .updateOne(
-          { _id: buildId },
-          {
-            $set: {
-              game: req.body.game,
-              gameVersion: req.body.gameVersion,
-              buildName: req.body.buildName,
-              civilizations: req.body.civilizations,
-              maps: req.body.maps,
-              postedBy: req.body.postedBy,
-              postedDate: req.body.postedDate,
-              steps: req.body.steps,
-              videoExample: req.body.videoExample,
-            },
-          }
-        );
-      if (result.acknowledged) {
-        res.status(200).json(result);
-        next();
+      if (
+        !req.body.game ||
+        !req.body.gameVersion ||
+        !req.body.buildName ||
+        !req.body.civilizations ||
+        !req.body.maps ||
+        !req.body.postedBy ||
+        !req.body.postedDate ||
+        !req.body.steps
+      ) {
+        res.status(400).json("Missing content in one or more required fields.");
       } else {
-        res.status(400).json("A problem occurred when trying to update build.");
+        const result = await mongodb
+          .getDb()
+          .db()
+          .collection("build-orders")
+          .updateOne(
+            { _id: buildId },
+            {
+              $set: {
+                game: req.body.game,
+                gameVersion: req.body.gameVersion,
+                buildName: req.body.buildName,
+                civilizations: req.body.civilizations,
+                maps: req.body.maps,
+                postedBy: req.body.postedBy,
+                postedDate: req.body.postedDate,
+                steps: req.body.steps,
+                videoExample: req.body.videoExample,
+              },
+            }
+          );
+        if (result.acknowledged) {
+          res.status(200).json(result);
+          next();
+        } else {
+          res
+            .status(400)
+            .json("A problem occurred when trying to update build.");
+        }
       }
     }
   } catch (err) {
