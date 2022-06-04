a9c7345430a6abb7d5b259f83bc605f97ed7ee6e
Daniel Eckton <dseckton@gmail.com>
2022-06-03 18:38:13 -0600

    adding authentication and login

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongodb = require("./db/connect");
+const { requiresAuth } = require("express-openid-connect");
+
 // TODO: Add routes here
 const buildOrders = require("./routes/builds");
 
@@ -8,7 +10,26 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
+
+const { auth } = require("express-openid-connect");
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
 
 app
   .use(bodyParser.json())
@@ -17,8 +38,16 @@ app
     next();
   })
   .use("/builds", buildOrders)
+  .use("/profile", requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  })
   // .use("/users", users)
-  .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  .use(
+    "/api-docs",
+    requiresAuth(),
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+  );
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
